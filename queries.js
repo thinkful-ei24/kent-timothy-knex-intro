8b2104d62ec63c2aebb503bc9ac46661939ebcee
Kent Tokunaga <kenttokunaga@gmail.com>
2018-09-11 11:28:03 -0400

    First 3 exercies

--- queries.js
+++ queries.js
@@ -7,9 +7,33 @@ const knex = require('knex')(DATABASE);
 process.stdout.write('\x1Bc');
 
 // Sample select 
-knex
-  .select()
-  .from('restaurants')
-  .limit(2)
-  .debug(true)
-  .then(results => console.log(results));
+// knex
+//   .select()
+//   .from('restaurants')
+//   .limit(2)
+//   .debug(true)
+//   .then(results => console.log(results));
+
+// knex.select('id', 'name', 'borough', 'cuisine')
+//   .from('restaurants')
+//   .where( { borough: 'Manhattan'} )
+//   .debug(true)
+//   .then(results => console.log(results));
+
+// 1. Get all restaurants
+// knex.select()
+//   .from('restaurants')
+//   .then(results => console.log(results));
+
+// 2. Get Italian restaurants
+// knex.select()
+//   .from('restaurants')
+//   .where({cuisine: 'Italian'})
+//   .then(results => console.log(results));
+
+// 3. Get 10 Italian restaurants, subset of fields
+// knex.select('id', 'name')
+//   .from('restaurants')
+//   .where({cuisine: 'Italian'})
+//   .limit(10)
+//   .then(results => console.log(results));
